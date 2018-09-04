879c076813a6f90da4bd4f0f8350c451cdee90cb
Monmoy042 <monmoykms.16@gmail.com>
2018-09-04 09:47:27 +0600

    another home security system added

---
+++ Second Version/arduino_Home_Security/arduino_Home_Security.ino
@@ -0,0 +1,112 @@
+#include<Servo.h>
+#define REC 2 // pin 2 is used for recording
+#define PLAY_E 3 // pin 3 is used for playback-edge trigge
+#define PLAY_L 4 // pin 4 is used for playback  
+#define FT 5 // pin 5 is used for feed through
+#define playTime 5000 // playback time 5 seconds
+#define recordTime 3000 // recording time 3 seconds
+#define playLTime 900 // press and release playback time 0.9 seconds
+
+const int analogPin = A0;    // Flame Sensor (A0) to Arduino analog input pin A0
+//const int BuzzerPin = 13;
+const int motorPin1  = 9;  // Pin 14 of L293
+const int motorPin2  = 10; // Buzzer output pin
+const int threshold = 720;// Flame level threshold (You can vary the value depends on your need)
+
+Servo servo1;
+int pos1=0;
+
+void setup() 
+{
+  pinMode(motorPin1, OUTPUT);
+  pinMode(motorPin2, OUTPUT); 
+  pinMode(REC,OUTPUT);// set the REC pin as output
+  pinMode(PLAY_L,OUTPUT);// set the PLAY_L pin as output
+  pinMode(PLAY_E,OUTPUT);// set the PLAY_e pin as output
+  pinMode(FT,OUTPUT);// set the FT pin as output  
+  servo1.attach(8);
+  Serial.begin(9600);// set up Serial monitor   
+}
+
+void loop() {
+  recorder();
+  
+  exhaustFan();
+ 
+  largeServo();
+}
+//Recorder
+void recorder(){
+  while (Serial.available() > 0) {
+          char inChar = (char)Serial.read();
+            if(inChar =='p' || inChar =='P'){
+            digitalWrite(PLAY_E, HIGH);
+            digitalWrite(PLAY_E, LOW);  
+              //Serial.println("Playbak Started");  
+            delay(playTime);
+            
+              //Serial.println("Playbak Ended");
+            break; 
+            }// if          
+            else if(inChar =='r' || inChar =='R'){
+              digitalWrite(REC, HIGH);
+              //Serial.println("Recording started");
+              delay(recordTime);
+              digitalWrite(REC, LOW);
+              //Serial.println("Recording Stopped ");              
+            } 
+            else if(inChar =='l' || inChar =='L'){
+            digitalWrite(PLAY_L, HIGH); 
+              //Serial.println("Playbak L Started");  
+            delay(playLTime);
+            digitalWrite(PLAY_L, LOW);
+              //Serial.println("Playbak L Ended");            
+            }             
+            
+      //Serial.println("**** Serial Monitor Exited");      
+    }// wihile
+    //Serial.println("**** Enter r to record, p to play");
+  }
+//Exhaust Fan
+void exhaustFan(){
+  int analogValue = analogRead(analogPin);
+ Serial.println(analogValue);
+  
+  if (analogValue>threshold) {
+    //digitalWrite(BuzzerPin, HIGH);
+    digitalWrite(motorPin1, HIGH);
+    digitalWrite(motorPin2, LOW);
+    delay(500);
+    Serial.println(analogValue);
+  } 
+  else if (analogValue = threshold){
+    
+    //digitalWrite(BuzzerPin, LOW);
+    digitalWrite(motorPin1, LOW);
+    digitalWrite(motorPin2, LOW);
+    //Serial.println(analogValue);
+    delay(400);
+    //digitalWrite(BuzzerPin, LOW);
+  }
+  else {
+    //digitalWrite(BuzzerPin, LOW);
+    digitalWrite(motorPin1, LOW);
+    digitalWrite(motorPin2, LOW); 
+  }
+  }
+
+//Large Servo
+
+void largeServo(){
+  for(pos1==0;pos1<180;pos1+=1)
+  {
+    servo1.write(pos1);
+    delay(50);
+    }
+    
+    for(pos1==180;pos1>=1;pos1-=1)
+  {
+    servo1.write(pos1);
+    delay(50);
+    }
+  }
---
+++ Second Version/my_code/my_code.ino
@@ -0,0 +1,207 @@
+//Important Libries
+#define CAYENNE_PRINT Serial
+#include <CayenneMQTTESP8266.h>
+#include <ESP8266WiFi.h>
+
+char ssid[] = "anirban-wifi"; //Your Wifi Network Name
+char wifiPassword[] = "amijanina"; //Your Wifi Password
+
+char username[] = "a60212f0-deff-11e7-b67f-67bba9556416"; //This User Name From Cayenne Website
+char password[] = "6072d05a87444f2217f11bd7f002dd7a5057f621"; //This Password From Cayenne Website
+char clientID[] = "c1970cf0-a9ca-11e8-9207-895fcb08b269"; //This Client ID From Cayenne Website
+
+//Motion
+int ledpin1 = 12;// Digital pin n D6
+int ledpin2 =14;//digital pin D5
+int pir1;
+int pir2;
+int sensor1 = 13;//motion senson1 on D7
+int sensor2 =15; //motion sensor2 on D8;
+
+//Vibration
+int vibration = 16; //vibration sensor D0
+int LedPin3 =5;  //vibration in led D1
+
+//Smoke Sensor
+const int smoke = A0;
+const int Buzzerpin = 2; //bUZZ PIN D4
+
+const int threshold = 190;//gas sensor
+
+//Panic button
+
+int button = 10; // panic on D3
+int button_value = 0;
+int ledpin4 =4;//digital pin D2
+
+void setup() {
+
+  //Motion
+  pinMode(sensor1, INPUT);   // declare sensor as input
+  pinMode(sensor2, INPUT);
+  pinMode(ledpin1, OUTPUT);  // declare LED as output
+  pinMode(ledpin2, OUTPUT);
+
+  //Smoke
+  pinMode(smoke, INPUT);
+  pinMode(Buzzerpin, OUTPUT); //Buzzpin for fire alarm
+
+  //Vibration
+  pinMode(vibration, INPUT);
+  pinMode(LedPin3, OUTPUT);
+
+  //Panic button
+  pinMode(ledpin4, OUTPUT);
+  pinMode(button, INPUT);
+
+  
+  Serial.begin(115200);
+  Cayenne.begin(username, password, clientID, ssid, wifiPassword);
+}
+
+void loop() {
+  Cayenne.loop();
+  
+  gasSensor();
+  
+  vibration_sensor();
+}
+
+//gas sensor
+void gasSensor(){ 
+  int analogValue = analogRead(smoke);
+
+   Serial.println("\n gas = ");
+  Serial.println(analogValue);
+  
+  if (analogValue > threshold) {
+    digitalWrite(Buzzerpin, HIGH);
+  }
+  else if (analogValue == threshold) {
+
+    digitalWrite(Buzzerpin, LOW);
+    delay(400);
+    digitalWrite(Buzzerpin, LOW);
+  }
+  else {
+    digitalWrite(Buzzerpin, LOW);
+  }
+
+  Cayenne.virtualWrite(0, analogRead(A0));
+  Serial.println(analogRead(A0));
+  delay(500);
+}
+
+////Motion Sensor Detection
+//void motionDetected1(){
+//  pir1 = digitalRead (sensor1);
+//  digitalWrite(ledpin1, pir1);
+//  Serial.println("\n motion 1 = ");
+//  Serial.println(pir1);
+//  Cayenne.virtualWrite(1, digitalRead(13));
+//  //Serial.println(digitalRead(13));
+//  delay(500);
+//}
+
+//void motionDetected2(){  
+//  pir2=digitalRead(sensor2);
+//  digitalWrite(ledpin2, pir2);
+//  Serial.println("\n motion 2 = ");
+//  Serial.println(pir2);
+//  Cayenne.virtualWrite(2, digitalRead(15));
+//  //Serial.println(digitalRead(15));
+//  delay(500);
+//  }
+
+////Panic Button
+//void Panic_Button(){
+//  button_value = digitalRead(button);
+//
+//  Serial.println("\n Panic = ");
+//  Serial.println(button_value);
+//  
+//  if (button_value == 1)
+//  {
+//    digitalWrite(ledpin4, HIGH);
+//
+//    //Serial.println("\n Panic = ");
+//    //Serial.println(button_value);
+//    delay(200);
+//    digitalWrite(ledpin4, LOW);
+//  }
+//
+//  else if (button_value == 0)
+//  {
+//    digitalWrite(ledpin4, LOW);
+//
+//    //Serial.println("\n Panic = ");
+//    //Serial.println(button_value);
+//    delay(200);
+//  }
+//
+//  Cayenne.virtualWrite(3, digitalRead(10));
+//  Serial.println(digitalRead(10));
+//  delay(500);
+//}
+
+
+// Vibration Sensor
+ void vibration_sensor(){
+  int vibrationvalue = digitalRead(vibration);
+
+  if (vibrationvalue == HIGH)  {
+    digitalWrite(LedPin3, HIGH);
+    Serial.print("\n Vibration");
+    delay(1000);
+  } 
+
+  else {
+    digitalWrite(LedPin3, LOW);
+    Serial.print("\n NO Vibration");
+    delay(1000);
+    }
+  Cayenne.virtualWrite(4, digitalRead(16));
+  //Serial.println(digitalRead(16));
+  delay(500);
+}
+
+
+//CAYENNE_IN(1)
+//{ CAYENNE_LOG("CAYENNE_IN_DEFAULT(%u) - %s, %s", request.channel, getValue.getId(), getValue.asString());
+//
+//  int i = getValue.asInt();
+//  digitalWrite(13, i);
+//}
+//
+//
+//CAYENNE_IN(2)
+//{ CAYENNE_LOG("CAYENNE_IN_DEFAULT(%u) - %s, %s", request.channel, getValue.getId(), getValue.asString());
+//
+//  int j = getValue.asInt();
+//  digitalWrite(15, j);
+//}
+//
+//
+//CAYENNE_IN(3)
+//{ CAYENNE_LOG("CAYENNE_IN_DEFAULT(%u) - %s, %s", request.channel, getValue.getId(), getValue.asString());
+//
+//  int k = getValue.asInt();
+//  digitalWrite(10, k);
+//}
+ 
+//CAYENNE_IN(4)
+//{ CAYENNE_LOG("CAYENNE_IN_DEFAULT(%u) - %s, %s", request.channel, getValue.getId(), getValue.asString());
+//
+//  int l = getValue.asInt();
+//  digitalWrite(16, l);
+//}
+
+CAYENNE_IN_DEFAULT()
+{
+  CAYENNE_LOG("CAYENNE_IN_DEFAULT(%u) - %s, %s", request.channel, getValue.getId(), getValue.asString());
+  //Process message here. If there is an error set an error message using getValue.setError(), e.g getValue.setError("Error message");
+
+   int i = getValue.asInt();
+   digitalWrite(16, i);
+ 
+}
